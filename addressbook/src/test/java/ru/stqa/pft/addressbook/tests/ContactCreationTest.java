846541ad831c5b258a222347edbcdafce2ea816d
Valerich1804 <a-poluektov@yandex.ru>
2021-08-03 23:41:11 +0300

    Добавил разбивку appmanager и testbase

---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTest extends TestBase {
+
+  @Test
+  public void testUntitledTestCase() {
+    app.addNewContact();
+    app.feelContactForm(new ContactData("Andrew", "Valerevich", "Ivanov", "valerich1804", "NN Minina 12", "88991122", "valerich1804@yandex.ru"));
+    app.submitContactCreation();
+    app.gotoHomePage();
+
+  }
+}
