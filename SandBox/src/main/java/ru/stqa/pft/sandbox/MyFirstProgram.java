e361110063acc74cf6bf8724f8531817ed6ef87b
Valerich1804 <a-poluektov@yandex.ru>
2021-08-03 19:36:56 +0300

    Сделал чтобы расстояние считалось

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-    Point b = new Point(1.0,4.5);
-      System.out.println("расстояние между двумя точками X и Y = " + b.distance(b));
+    Point b = new Point(-1,4);
+    Point c = new Point(15,-48);
+      System.out.println("расстояние между двумя точками X и Y = " + b.distance(c));
 
     }
 }
